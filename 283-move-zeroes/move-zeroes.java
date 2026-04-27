b6770cc5aaed80dcff269eca41e64d44514f6360
SJSharath <152773110+SJSharath@users.noreply.github.com>
2026-04-27 20:26:06 +0530

    Time: 15 ms (6.46%) | Memory: 48.3 MB (7.55%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,14 +1,30 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int j = 0;
+        List<Integer> list = new ArrayList<>();
+        
+        // convert array to list
+        for (int num : nums) {
+            list.add(num);
+        }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                j++;
+        int x = Collections.frequency(list, 0);
+        if (x == 0) return;
+
+        int i = 0;
+        int b = list.size() - x;
+
+        while (i < b) {
+            if (list.get(i) == 0) {
+                int z = list.remove(i);  // pop(i)
+                list.add(z);             // append
+            } else {
+                i++;
             }
+        }
+
+        // copy back to array
+        for (int j = 0; j < nums.length; j++) {
+            nums[j] = list.get(j);
         }
     }
 }
